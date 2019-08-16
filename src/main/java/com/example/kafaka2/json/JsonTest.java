1590860bc8868da4ba48879392029aada54a4724
“ <1660669610@qq.com>
2019-08-16 14:10:52 +0800

    master  3

--- src/main/java/com/example/kafaka2/json/JsonTest.java
+++ src/main/java/com/example/kafaka2/json/JsonTest.java
@@ -1,9 +1,5 @@
 package com.example.kafaka2.json;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.JSONPObject;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 
@@ -13,7 +9,7 @@ import java.util.*;
  * Date: 2019/8/12
  * Time: 19:31
  */
-@Slf4j
+
 public class JsonTest {
 
     public static void main(String[] args) {
@@ -32,13 +28,7 @@ public class JsonTest {
         list.add(2);
         System.out.println(list);
         String a = null;
-        String securityPrefix = JSONPObject.SECURITY_PREFIX;
-        Object parse = JSON.parse("1");
-        //JSONArray objects = JSONArray.parseArray("[{"a":"1"}]");
-        //Object object = JSONObject.parse("OBJECT");
-        String string = JSONObject.toJSONString(list);
 
-        log.info("jsonp:"+securityPrefix+"json:"+parse+"jsono:"+"JSONObject.toJSONString(list):"+string);
 
         Map<String,Object> map = new HashMap<>();
 //        map.put("1","1");
